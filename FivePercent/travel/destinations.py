a47cd031f7ef072c27046b3325eca6b0a00be4cd
XavierF-QUT <n11037393@qut.edu.au>
2025-05-22 16:40:20 +1000

    Tutorial 9 Task 1 + 2

--- FivePercent/travel/destinations.py
+++ FivePercent/travel/destinations.py
@@ -1,47 +1,50 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from .models import Destination, Comment
 from .forms import DestinationForm, CommentForm
+from . import db
 
 destbp = Blueprint('destination', __name__, url_prefix='/destinations')
 
 @destbp.route('/<id>')
 def show(id):
-    destination = get_destination()
+    destination = db.session.scalar(db.select(Destination).where(Destination.id==id))
     # create the comment form
     cform = CommentForm()    
     return render_template('destinations/show.html', destination=destination, form=cform)
 
-@destbp.route('/create', methods = ['GET', 'POST'])
+@destbp.route('/create', methods=['GET', 'POST'])
 def create():
   print('Method type: ', request.method)
   form = DestinationForm()
   if form.validate_on_submit():
-    print('Successfully created new travel destination')
+    destination = Destination(name=form.name.data,
+    description=form.description.data,
+    image=form.image.data,
+    currency=form.currency.data)
+    # add the object to the db session
+    db.session.add(destination)
+    # commit to the database
+    db.session.commit()
+    print('Successfully created new travel destination', 'success')
+    # Always end with redirect when form is valid
     return redirect(url_for('destination.create'))
   return render_template('destinations/create.html', form=form)
 
-@destbp.route('/<id>/comment', methods = ['GET', 'POST'])
-def comment(id):
-  # here the form is created  form = CommentForm()
-  form = CommentForm()
-  if form.validate_on_submit():	#this is true only in case of POST method
-    print(f"The following comment has been posted: {form.text.data}")
-  # notice the signature of url_for
-  return redirect(url_for('destination.show', id=1))
-
-def get_destination():
-  # creating the description of Brazil
-  b_desc = """Brazil is considered an advanced emerging economy.
-   It has the ninth largest GDP in the world by nominal, and eight by PPP measures. 
-   It is one of the world\'s major breadbaskets, being the largest producer of coffee for the last 150 years."""
-   # an image location
-  image_loc = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFyC8pBJI2AAHLpAVih41_yWx2xxLleTtdshAdk1HOZQd9ZM8-Ag'
-  destination = Destination('Brazil', b_desc,image_loc, 'R$10')
-  # a comment
-  comment = Comment("Sam", "Visited during the olympics, was great", '2023-08-12 11:00:00')
-  destination.set_comments(comment)
-  comment = Comment("Bill", "free food!", '2023-08-12 11:00:00')
-  destination.set_comments(comment)
-  comment = Comment("Sally", "free face masks!", '2023-08-12 11:00:00')
-  destination.set_comments(comment)
-  return destination
+@destbp.route('/<id>/comment', methods=['GET', 'POST'])  
+def comment(id):  
+    form = CommentForm()  
+    # get the destination object associated to the page and the comment
+    destination = db.session.scalar(db.select(Destination).where(Destination.id==id))
+    if form.validate_on_submit():  
+      # read the comment from the form, associate the Comment's destination field
+      # with the destination object from the above DB query
+      comment = Comment(text=form.text.data, destination=destination) 
+      # here the back-referencing works - comment.destination is set
+      # and the link is created
+      db.session.add(comment) 
+      db.session.commit() 
+      # flashing a message which needs to be handled by the html
+      # flash('Your comment has been added', 'success')  
+      print('Your comment has been added', 'success') 
+    # using redirect sends a GET request to destination.show
+    return redirect(url_for('destination.show', id=id))
