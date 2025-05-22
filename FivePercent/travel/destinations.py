6e482121838c3adc17b90d52b0cc43a657d1ae82
XavierF-QUT <n11037393@qut.edu.au>
2025-05-22 16:42:42 +1000

    Tutorial 9 Task 3 + 4

--- FivePercent/travel/destinations.py
+++ FivePercent/travel/destinations.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from .models import Destination, Comment
 from .forms import DestinationForm, CommentForm
 from . import db
+import os
+from werkzeug.utils import secure_filename
 
 destbp = Blueprint('destination', __name__, url_prefix='/destinations')
 
@@ -17,10 +19,10 @@ def create():
   print('Method type: ', request.method)
   form = DestinationForm()
   if form.validate_on_submit():
-    destination = Destination(name=form.name.data,
-    description=form.description.data,
-    image=form.image.data,
-    currency=form.currency.data)
+    # call the function that checks and returns image
+    db_file_path = check_upload_file(form)
+    destination = Destination(name=form.name.data,description=form.description.data, 
+    image = db_file_path,currency=form.currency.data)
     # add the object to the db session
     db.session.add(destination)
     # commit to the database
@@ -30,19 +32,33 @@ def create():
     return redirect(url_for('destination.create'))
   return render_template('destinations/create.html', form=form)
 
+def check_upload_file(form):
+  # get file data from form  
+  fp = form.image.data
+  filename = fp.filename
+  # get the current path of the module file… store image file relative to this path  
+  BASE_PATH = os.path.dirname(__file__)
+  # upload file location – directory of this file/static/image
+  upload_path = os.path.join(BASE_PATH,'static/image',secure_filename(filename))
+  # store relative path in DB as image location in HTML is relative
+  db_upload_path = '/static/image/' + secure_filename(filename)
+  # save the file and return the db upload path  
+  fp.save(upload_path)
+  return db_upload_path
+
 @destbp.route('/<id>/comment', methods=['GET', 'POST'])  
 def comment(id):  
     form = CommentForm()  
     # get the destination object associated to the page and the comment
     destination = db.session.scalar(db.select(Destination).where(Destination.id==id))
     if form.validate_on_submit():  
-      # read the comment from the form, associate the Comment's destination field
-      # with the destination object from the above DB query
+      # read the comment from the form
       comment = Comment(text=form.text.data, destination=destination) 
       # here the back-referencing works - comment.destination is set
       # and the link is created
       db.session.add(comment) 
       db.session.commit() 
+
       # flashing a message which needs to be handled by the html
       # flash('Your comment has been added', 'success')  
       print('Your comment has been added', 'success') 
--- FivePercent/travel/forms.py
+++ FivePercent/travel/forms.py
@@ -1,13 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
 from wtforms.validators import InputRequired, Email, EqualTo
+from flask_wtf.file import FileRequired, FileField, FileAllowed
+
+ALLOWED_FILE = {'PNG', 'JPG', 'JPEG', 'png', 'jpg', 'jpeg'}
 
 # Create new destination
 class DestinationForm(FlaskForm):
   name = StringField('Country', validators=[InputRequired()])
   description = TextAreaField('Description', 
-            validators = [InputRequired()])
-  image = StringField('Cover Image', validators=[InputRequired()])
+            validators=[InputRequired()])
+  image = FileField('Destination Image', validators=[
+    FileRequired(message = 'Image cannot be empty'),
+    FileAllowed(ALLOWED_FILE, message='Only supports png, jpg, JPG, PNG')])
   currency = StringField('Currency', validators=[InputRequired()])
   submit = SubmitField("Create")
     
